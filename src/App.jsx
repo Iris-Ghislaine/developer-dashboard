401937243c0c5ad1f87f9f55a30ceb61badd2d51
Iris-Ghislaine <ganzairis03@gmail.com>
2025-10-25 09:02:59 +0200

    Added Navbar component with theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+
+  <Router>
+     <Navbar />
+    <Routes>
+      <Route>
+
+      </Route>
+    </Routes>
+  </Router>
+    
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+import ThemeToggle from './ThemeToogle';
+import { Code2 } from 'lucide-react';
+
+const Navbar = ({ onToggleTheme, currentTheme }) => {
+
+  return (
+ <nav className="w-full  bg-indigo-50 fixed transition-smooth">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <div  className="flex items-center space-x-2 group">
+            <div className="bg-gradient-to-r from-indigo-500 to-violet-500 p-2 rounded-lg transition-transform group-hover:scale-110">
+              <Code2 className="h-6 w-6 text-white" />
+            </div>
+            <span className="text-xl font-bold bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text text-transparent">
+              Developer Dashboard
+            </span>
+          </div>
+
+          <div className="flex items-center space-x-3 flex-shrink-0">
+            <ThemeToggle onToggle={onToggleTheme} currentTheme={currentTheme} />
+       
+          </div>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
